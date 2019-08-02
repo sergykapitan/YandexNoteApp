0cfd55e9817cec58b4818ce1ca2e80dd10d4e0a3
Sergey <12kaptan12@gmail.com>
2019-08-02 23:42:03 +0300

    add RemoveNoteOperation

--- TestApp/Operation/RemoveNoteOperation.swift
+++ TestApp/Operation/RemoveNoteOperation.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+import CocoaLumberjack
+
+class RemoveNoteOperation: AsyncOperation {
+    
+    private let noteId: String
+    private var saveToBackend: SaveNotesBackendOperation
+    private let removeFromDb: RemoveNoteDBOperation
+    
+    init(noteId: String,
+         notebook: FileNotebook,
+         backendQueue: OperationQueue,
+         dbQueue: OperationQueue) {
+        
+        self.noteId = noteId
+        
+        removeFromDb = RemoveNoteDBOperation(noteId: noteId, notebook: notebook)
+        saveToBackend = SaveNotesBackendOperation(notes: notebook.notes)
+        
+        super.init()
+        
+        removeFromDb.completionBlock = {
+            backendQueue.addOperation(self.saveToBackend)
+        }
+        
+        addDependency(removeFromDb)
+        addDependency(saveToBackend)
+        
+        dbQueue.addOperation(removeFromDb)
+    }
+    
+    override func main() {
+        DDLogDebug("Deleted note with ID \(noteId)")
+        
+        finish()
+    }
+}
